0a33ea6ac94e4c22010e5d4d4da87d9c272fe100
kwakhoyeong <ghy2002@hanmail.net>
2021-04-17 10:44:46 +0900

    2138_전구와 스위치 수정판

--- python/baekjoon/2138_전구와스위치.py
+++ python/baekjoon/2138_전구와스위치.py
@@ -2,13 +2,59 @@
 https://jshong1125.tistory.com/25
 '''
 import sys
+readl = sys.stdin.readline
+n = int(readl().strip())
+
+curLamp = list(map(int,readl().strip()))
+reLamp = list(map(int, readl().strip()))
+
+def change(n):
+    if n == 1:
+        n = 0
+    else:
+        n = 1
+    return n
+
+def switch(lst, cnt):
+    count = cnt
+    if count == 1:
+        lst[0] = change(lst[0])
+        lst[1] = change(lst[1])
+
+    for i in range(1, n):
+        if lst[i-1] != reLamp[i-1]:
+            lst[i-1] = change(lst[i-1])
+            count += 1
+            lst[i] = change(lst[i])
+            if i != n-1:
+                lst[i+1] = change([i+1])
+    if lst == reLamp:
+        return count
+    else:
+        return -1
+
+res1 = switch(curLamp[:], 0)
+res2 = switch(curLamp[:], 1)
+
+if res1 >= 0 and res2 >=0:
+    print(min(res1, res2))
+elif res1 >= 0 and res2 < 0:
+    print(res1)
+elif res1 < 0 and res2 >= 0:
+    print(res2)
+else:
+    print(-1)
+
+
+
+'''
+import sys
 
 readl = sys.stdin.readline
 N = int(readl().strip())
 
 currentLamp = list(map(int, readl().rstrip("\n")))
 resultLamp = list(map(int, readl().rstrip("\n")))
-
 def change(n):
     if n == 0:
         n = 1
@@ -16,20 +62,19 @@ def change(n):
         n = 0
     return n
 
-def switch(n, cnt):
+def switch(c, cnt):
     count = cnt
-
     if count == 1:
-        currentLamp[0] = change(currentLamp[0])
-        currentLamp[1] = change(currentLamp[1])
+        c[0] = change(c[0])
+        c[1] = change(c[1])
     for i in range(1, N):
-        if currentLamp[i-1] != resultLamp[i-1]:
+        if c[i-1] != resultLamp[i-1]:
             count += 1
-            currentLamp[i-1] = change(currentLamp[i-1])
-            currentLamp[i] = change(currentLamp[i])
+            c[i-1] = change(c[i-1])
+            c[i] = change(c[i])
             if i != N-1:
-                currentLamp[i+1] = change(currentLamp[i+1])
-    if currentLamp == resultLamp:
+                c[i+1] = change(c[i+1])
+    if c == resultLamp:
         return count
     else:
         return -1
@@ -38,10 +83,11 @@ res1 = switch(currentLamp[:], 0)
 res2 = switch(currentLamp[:], 1)
 
 if res1 >= 0 and res2 >= 0:
-    print(min(res1,res2))
+    print(min(res1, res2))
 elif res1 >= 0 and res2 < 0:
     print(res1)
 elif res1 < 0 and res2 >= 0:
     print(res2)
 else:
     print(-1)
+'''
