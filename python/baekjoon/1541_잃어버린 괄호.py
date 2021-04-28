fb08b10d841b8671f4bf7a92e170deb2f6135b8f
hoyeong.kwak <ghy2002@hanmail.net>
2021-04-28 09:20:00 +0900

    1541 잃어버린 괄호

---
+++ python/baekjoon/1541_잃어버린 괄호.py
@@ -0,0 +1,17 @@
+import sys
+
+readl = sys.stdin.readline
+n = readl().split("-")
+num = []
+
+for i in n:
+    cnt = 0
+    s = i.split("+")
+    for j in s:
+        cnt += int(j)
+    num.append(cnt)
+n = num[0]
+for i in range(1, len(num)):
+    n -= num[i]
+print(n)
+
